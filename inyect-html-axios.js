f3e011e14c9734dcd7e9c698bd04a2f8e466e83f
Dev Front <developermater26@gmail.com>
2021-04-13 20:20:43 -0600

    se llamaron los elementos a modificar y se recorrio para obtener el elemento y el url

--- inyect-html-axios.js
+++ inyect-html-axios.js
@@ -1,4 +1,14 @@
 const InyectHTML = ( )=>{
 
+
+
+
+
+
+
+
+    
+    let $reference = document.querySelectorAll('[data-content]')
+    $reference.forEach((el) => Controler(el,el.dataset.content))
 } 
 document.addEventListener('DOMContentLoaded',(e) => InyectHTML())
