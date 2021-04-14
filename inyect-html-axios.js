5a9a166fbfcdca309ff4232a0c166d77ef3d5b94
Dev Front <developermater26@gmail.com>
2021-04-13 20:26:27 -0600

    se creo una funcion asincrona que controla la peticion que se le aga pasando como parametros el 'elemto' y la 'url' y manejando el erro en un mensaje y el exito en el cambio de elemntos

--- inyect-html-axios.js
+++ inyect-html-axios.js
@@ -1,13 +1,26 @@
 const InyectHTML = ( )=>{
 
 
+    const Controler = async (el,url)=>{
 
+        try {
 
+            let options = {
+                method:'GET',
+                headers:{
+                    'Content-Type':'text/html'
+                }
+            }
 
+            let res =await axios(url,options)
+            el.outerHTML = res.data
 
+        } catch (err) {
+            el.outerHTML = `${err.response.status} An Error occurred please check the references`
+        }
 
+    }
 
-    
     let $reference = document.querySelectorAll('[data-content]')
     $reference.forEach((el) => Controler(el,el.dataset.content))
 } 
